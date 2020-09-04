c9225eb0abc3658ec95dfc1c9cb81b4d6ea0c562
心叶 <1329132389@qq.com>
2020-09-04 10:10:28 +0800

    安装配置ant移动端UI库并使用跑马灯

--- src/pages/home/index/index.js
+++ src/pages/home/index/index.js
@@ -1,16 +1,42 @@
 import React from "react";
 import Css from "../../../assets/css/home/index/index.module.css";
+import { Carousel } from 'antd-mobile';
 
 class IndexComponent extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            data: ['1', '2', '3'],
+            imgHeight: 176,
+        }
     }
 
     componentDidMount() {
+        // simulate img loading
+        setTimeout(() => {
+            // this.setState({
+                // data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
+            // });
+        }, 100);
+    }
+
+    beforeChange(from, to) {
+        console.log(`slide from ${from} to ${to}`)
+    }
+
+    afterChange(index) {
+        console.log('slide to', index)
+    }
+    imgOnload() {
+        // fire window resize event to change height
+        window.dispatchEvent(new Event('resize'));
+        this.setState({ imgHeight: 'auto' });
     }
 
     render() {
+        // 轮播图外框样式
+        const swiperBox = { display: 'inline-block', width: '100%', height: this.state.imgHeight }
+        const swiperImage = { width: '100%', verticalAlign: 'top' }
         return (
             <div>
                 {/*头部搜索栏*/}
@@ -26,6 +52,27 @@ class IndexComponent extends React.Component {
                 </div>
                 {/*轮播图*/}
                 <div className={Css['swiper-wrap']}>
+                    <Carousel
+                        autoplay={false}
+                        infinite
+                        beforeChange={this.beforeChange.bind(this)}
+                        afterChange={this.afterChange.bind(this)}
+                    >
+                        {this.state.data.map(val => (
+                            <a
+                                key={val}
+                                href="http://www.baidu.com"
+                                style={swiperBox}
+                            >
+                                <img
+                                    src={`http://vueshop.glbuys.com/uploadfiles/1484285302.jpg`}
+                                    alt=""
+                                    style={swiperImage}
+                                    onLoad={this.imgOnload.bind(this)}
+                                />
+                            </a>
+                        ))}
+                    </Carousel>
                 </div>
             </div>
         );
