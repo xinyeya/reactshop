393b802e0ab4204c187c2e603bf5e79d25ad7ea9
心叶 <1329132389@qq.com>
2020-09-06 23:07:20 +0800

    完成分类栏点击切换路由参数与样式切换

--- src/pages/home/goods/classify.js
+++ src/pages/home/goods/classify.js
@@ -1,33 +1,74 @@
 /*eslint-disable*/
 import React, {lazy} from "react";
 import Css from "../../../assets/css/home/goods/classify.module.css"
-import {Route, Switch} from "react-router-dom";
+import {Route, Switch} from 'react-router-dom';
 import config from "../../../assets/js/conf/config";
 import IScroll from 'iscroll/build/iscroll-probe';
+import {request} from "../../../assets/js/libs/request";
 const GoodsItems = lazy(()=>import("./items"));
 
 class ClassifyComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            aClassify: null,
         };
         this.scroll = null;
+        this.aTempClassify = [];
     }
+
 
     componentDidMount() {
-        this.eventScroll();
+        this.getClassifyData();
     }
 
+    // 返回上一层
     goBack() {
         this.props.history.goBack();
     }
 
+    // 兼容ios滑动条
     eventScroll() {
         // 阻止默认的点击事件执行
         document.getElementById("scroll-classify").addEventListener("touchmove", function(e) {e.preventDefault();}, false);
         this.scroll = new IScroll('#scroll-classify', {
+            scrollX: false,
+            scrollY: true,
+            preventDefault: false,
             mouseWheel: true, // 允许鼠标滚轮
         });
+    }
+
+    // 获取分类数据
+    getClassifyData() {
+        request(config.baseUrl+"/api/home/category/menu?token="+config.token).then(res=>{
+            if (res.code === 200) {
+                this.aTempClassify = res.data;
+                for (let i=0; i<this.aTempClassify.length; i++){
+                    this.aTempClassify[i].bActive = false;
+                }
+                this.setState({
+                    aClassify: this.aTempClassify
+                },()=>{
+                    this.eventScroll();
+                })
+            }
+        });
+    }
+
+    // 点击切换子路由
+    replacePage(pUrl) {
+        console.log(config.path+pUrl);
+        this.props.history.replace(config.path+pUrl);
+    }
+
+    // 改变选中分类颜色
+    changeStyle(pUrl, pIndex) {
+        for (let i=0; i<this.aTempClassify.length; i++) {
+            this.aTempClassify[i].bActive = false;
+        }
+        this.aTempClassify[pIndex].bActive = true;
+        this.replacePage(pUrl, pIndex);
     }
 
     render() {
@@ -43,40 +84,13 @@ class ClassifyComponent extends React.Component {
                     {/*左侧分类*/}
                     <div id="scroll-classify" className={Css['classify-wrap']}>
                         <div>
-                            <div className={Css['classify-item'] + " " + Css['active']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
-                            <div className={Css['classify-item']}>潮流女装</div>
+                            {
+                                this.state.aClassify !== null && this.state.aClassify.map((val, key)=>{
+                                    return (
+                                        <div key={key} className={val.bActive === true?Css['classify-item'] + " " + Css['active'] : Css['classify-item']} onClick={this.changeStyle.bind(this, `goods/classify/items?cid=${val.cid} `, key)}>{val.title}</div>
+                                    )
+                                })
+                            }
                         </div>
                     </div>
                     <div className={Css['goods-content']}>
