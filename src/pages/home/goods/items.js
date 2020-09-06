3b9a1f718f60c1b2ba6796a466e0544a70950be8
心叶 <1329132389@qq.com>
2020-09-06 10:09:18 +0800

    完成分类页面样式

--- src/pages/home/goods/items.js
+++ src/pages/home/goods/items.js
@@ -14,9 +14,166 @@ class GoodsItems extends React.Component {
 
     render() {
         return (
-            <React.Fragment>
-                右侧商品
-            </React.Fragment>
+            <div className={Css['goods-content-main']}>
+                <div className={Css['goods-wrap']}>
+                    {/*标题*/}
+                    <div className={Css['classify-name']}>裙装</div>
+                    {/*内容*/}
+                    <div className={Css['goods-item-wrap']}>
+                        <ul>
+                            <li className={Css['image']}>
+                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
+                            </li>
+                            <li>
+                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
+                            </li>
+                        </ul>
+                        <ul>
+                            <li className={Css['image']}>
+                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
+                            </li>
+                            <li>
+                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
+                            </li>
+                        </ul>
+                        <ul>
+                            <li className={Css['image']}>
+                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
+                            </li>
+                            <li>
+                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
+                            </li>
+                        </ul>
+                        <ul>
+                            <li className={Css['image']}>
+                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
+                            </li>
+                            <li>
+                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
+                            </li>
+                        </ul>
+                        <ul>
+                            <li className={Css['image']}>
+                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
+                            </li>
+                            <li>
+                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
+                            </li>
+                        </ul>
+                        <ul>
+                            <li className={Css['image']}>
+                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
+                            </li>
+                            <li>
+                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
+                            </li>
+                        </ul>
+                        <ul>
+                            <li className={Css['image']}>
+                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
+                            </li>
+                            <li>
+                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
+                            </li>
+                        </ul>
+                        <ul>
+                            <li className={Css['image']}>
+                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
+                            </li>
+                            <li>
+                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
+                            </li>
+                        </ul>
+                        <ul>
+                            <li className={Css['image']}>
+                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
+                            </li>
+                            <li>
+                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={Css['goods-wrap']}>
+                    {/*标题*/}
+                    <div className={Css['classify-name']}>裙装</div>
+                    {/*内容*/}
+                    <div className={Css['goods-item-wrap']}>
+                        <ul>
+                            <li className={Css['image']}>
+                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
+                            </li>
+                            <li>
+                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
+                            </li>
+                        </ul>
+                        <ul>
+                            <li className={Css['image']}>
+                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
+                            </li>
+                            <li>
+                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
+                            </li>
+                        </ul>
+                        <ul>
+                            <li className={Css['image']}>
+                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
+                            </li>
+                            <li>
+                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
+                            </li>
+                        </ul>
+                        <ul>
+                            <li className={Css['image']}>
+                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
+                            </li>
+                            <li>
+                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
+                            </li>
+                        </ul>
+                        <ul>
+                            <li className={Css['image']}>
+                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
+                            </li>
+                            <li>
+                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
+                            </li>
+                        </ul>
+                        <ul>
+                            <li className={Css['image']}>
+                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
+                            </li>
+                            <li>
+                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
+                            </li>
+                        </ul>
+                        <ul>
+                            <li className={Css['image']}>
+                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
+                            </li>
+                            <li>
+                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
+                            </li>
+                        </ul>
+                        <ul>
+                            <li className={Css['image']}>
+                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
+                            </li>
+                            <li>
+                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
+                            </li>
+                        </ul>
+                        <ul>
+                            <li className={Css['image']}>
+                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
+                            </li>
+                            <li>
+                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
