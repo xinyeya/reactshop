f317b3eaf68051cf7b20f1a93f4a5cf22206fda4
心叶 <1329132389@qq.com>
2020-09-06 22:34:31 +0800

    使用ISscroll兼容滚动条

--- src/pages/home/goods/classify.js
+++ src/pages/home/goods/classify.js
@@ -1,22 +1,33 @@
+/*eslint-disable*/
 import React, {lazy} from "react";
 import Css from "../../../assets/css/home/goods/classify.module.css"
 import {Route, Switch} from "react-router-dom";
 import config from "../../../assets/js/conf/config";
+import IScroll from 'iscroll/build/iscroll-probe';
 const GoodsItems = lazy(()=>import("./items"));
 
 class ClassifyComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            items: 100
-        }
+        };
+        this.scroll = null;
     }
 
     componentDidMount() {
+        this.eventScroll();
     }
 
     goBack() {
         this.props.history.goBack();
+    }
+
+    eventScroll() {
+        // 阻止默认的点击事件执行
+        document.getElementById("scroll-classify").addEventListener("touchmove", function(e) {e.preventDefault();}, false);
+        this.scroll = new IScroll('#scroll-classify', {
+            mouseWheel: true, // 允许鼠标滚轮
+        });
     }
 
     render() {
@@ -30,29 +41,43 @@ class ClassifyComponent extends React.Component {
                 {/*左侧*/}
                 <div className={Css['goods-main']}>
                     {/*左侧分类*/}
-                    <div className={Css['classify-wrap']}>
-                        <div className={Css['classify-item'] + " " + Css['active']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
+                    <div id="scroll-classify" className={Css['classify-wrap']}>
+                        <div>
+                            <div className={Css['classify-item'] + " " + Css['active']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                        </div>
                     </div>
                     <div className={Css['goods-content']}>
                         <Switch>
--- src/pages/home/goods/items.js
+++ src/pages/home/goods/items.js
@@ -1,3 +1,4 @@
+/*eslint-disable*/
 import React from "react";
 import config from "../../../assets/js/conf/config";
 import Css from "../../../assets/css/home/goods/items.module.css";
