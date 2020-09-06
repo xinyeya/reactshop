57047293a22897ec6c0b707b469efda60ae022d2
心叶 <1329132389@qq.com>
2020-09-06 23:44:19 +0800

    添加图片懒加载和get参数获取工具

---
+++ src/assets/js/utils/util.js
@@ -0,0 +1,29 @@
+import echo from '../libs/echo.js';
+function lazyImg(){
+    echo.init({
+        offset : 100,//可是区域多少像素可以被加载
+        throttle : 0 //设置图片延迟加载的时间
+    });
+}
+
+function localParam(search, hash) {
+    search = search || window.location.search;
+    hash = hash || window.location.hash;
+    var fn = function(str, reg) {
+        if (str) {
+            var data = {};
+            str.replace(reg, function($0, $1, $2, $3) {
+                data[$1] = $3;
+            });
+            return data;
+        }
+    }
+    return {
+        search : fn(search, new RegExp("([^?=&]+)(=([^&]*))?", "g")) || {},
+        hash : fn(hash, new RegExp("([^#=&]+)(=([^&]*))?", "g")) || {}
+    };
+}
+export {
+    lazyImg,
+    localParam
+}
--- src/pages/home/goods/items.js
+++ src/pages/home/goods/items.js
@@ -2,7 +2,7 @@
 import React from "react";
 import config from "../../../assets/js/conf/config";
 import Css from "../../../assets/css/home/goods/items.module.css";
-import {Route, Switch} from "react-router-dom";
+import IScroll from 'iscroll/build/iscroll-probe';
 import {request} from "../../../assets/js/libs/request";
 
 class GoodsItems extends React.Component {
@@ -11,6 +11,7 @@ class GoodsItems extends React.Component {
         this.state = {
             aGoods: []
         }
+        this.scroll = null;
     }
 
     componentDidMount() {
@@ -18,20 +19,35 @@ class GoodsItems extends React.Component {
         this.getData();
     }
 
+    // 获取数据
     getData() {
         request(config.baseUrl+"/api/home/category/show?cid=493&token="+config.token).then(res=>{
             if (res.code === 200) {
                 this.setState({
                     aGoods: res.data
+                }, ()=>{
+                    this.eventScroll();
                 });
                 console.log(res.data)
             }
         })
     }
 
+    // 兼容ios滑动条
+    eventScroll() {
+        // 阻止默认的点击事件执行
+        document.getElementById("goods-content-main").addEventListener("touchmove", function(e) {e.preventDefault();}, false);
+        this.scroll = new IScroll('#goods-content-main', {
+            scrollX: false,
+            scrollY: true,
+            preventDefault: false,
+            mouseWheel: true, // 允许鼠标滚轮
+        });
+    }
+
     render() {
         return (
-            <div className={Css['goods-content-main']}>
+            <div id={"goods-content-main"} className={Css['goods-content-main']}>
                 <div>
                     {
                         this.state.aGoods !== null && this.state.aGoods.map((val, key)=>{
