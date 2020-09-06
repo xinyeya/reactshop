df40849d7daf65df42bd6b2e55637aee88e0298c
心叶 <1329132389@qq.com>
2020-09-06 23:39:18 +0800

    完成右侧数据渲染

--- src/pages/home/goods/items.js
+++ src/pages/home/goods/items.js
@@ -3,176 +3,63 @@ import React from "react";
 import config from "../../../assets/js/conf/config";
 import Css from "../../../assets/css/home/goods/items.module.css";
 import {Route, Switch} from "react-router-dom";
+import {request} from "../../../assets/js/libs/request";
 
 class GoodsItems extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            aGoods: []
+        }
     }
 
     componentDidMount() {
+        console.log(this.props.location)
+        this.getData();
+    }
+
+    getData() {
+        request(config.baseUrl+"/api/home/category/show?cid=493&token="+config.token).then(res=>{
+            if (res.code === 200) {
+                this.setState({
+                    aGoods: res.data
+                });
+                console.log(res.data)
+            }
+        })
     }
 
     render() {
         return (
             <div className={Css['goods-content-main']}>
-                <div className={Css['goods-wrap']}>
-                    {/*标题*/}
-                    <div className={Css['classify-name']}>裙装</div>
-                    {/*内容*/}
-                    <div className={Css['goods-item-wrap']}>
-                        <ul>
-                            <li className={Css['image']}>
-                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
-                            </li>
-                            <li>
-                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
-                            </li>
-                        </ul>
-                        <ul>
-                            <li className={Css['image']}>
-                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
-                            </li>
-                            <li>
-                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
-                            </li>
-                        </ul>
-                        <ul>
-                            <li className={Css['image']}>
-                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
-                            </li>
-                            <li>
-                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
-                            </li>
-                        </ul>
-                        <ul>
-                            <li className={Css['image']}>
-                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
-                            </li>
-                            <li>
-                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
-                            </li>
-                        </ul>
-                        <ul>
-                            <li className={Css['image']}>
-                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
-                            </li>
-                            <li>
-                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
-                            </li>
-                        </ul>
-                        <ul>
-                            <li className={Css['image']}>
-                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
-                            </li>
-                            <li>
-                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
-                            </li>
-                        </ul>
-                        <ul>
-                            <li className={Css['image']}>
-                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
-                            </li>
-                            <li>
-                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
-                            </li>
-                        </ul>
-                        <ul>
-                            <li className={Css['image']}>
-                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
-                            </li>
-                            <li>
-                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
-                            </li>
-                        </ul>
-                        <ul>
-                            <li className={Css['image']}>
-                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
-                            </li>
-                            <li>
-                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div className={Css['goods-wrap']}>
-                    {/*标题*/}
-                    <div className={Css['classify-name']}>裙装</div>
-                    {/*内容*/}
-                    <div className={Css['goods-item-wrap']}>
-                        <ul>
-                            <li className={Css['image']}>
-                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
-                            </li>
-                            <li>
-                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
-                            </li>
-                        </ul>
-                        <ul>
-                            <li className={Css['image']}>
-                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
-                            </li>
-                            <li>
-                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
-                            </li>
-                        </ul>
-                        <ul>
-                            <li className={Css['image']}>
-                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
-                            </li>
-                            <li>
-                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
-                            </li>
-                        </ul>
-                        <ul>
-                            <li className={Css['image']}>
-                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
-                            </li>
-                            <li>
-                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
-                            </li>
-                        </ul>
-                        <ul>
-                            <li className={Css['image']}>
-                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
-                            </li>
-                            <li>
-                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
-                            </li>
-                        </ul>
-                        <ul>
-                            <li className={Css['image']}>
-                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
-                            </li>
-                            <li>
-                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
-                            </li>
-                        </ul>
-                        <ul>
-                            <li className={Css['image']}>
-                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
-                            </li>
-                            <li>
-                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
-                            </li>
-                        </ul>
-                        <ul>
-                            <li className={Css['image']}>
-                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
-                            </li>
-                            <li>
-                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
-                            </li>
-                        </ul>
-                        <ul>
-                            <li className={Css['image']}>
-                                <img src={"//vueshop.glbuys.com/uploadfiles/1523793417.jpg"} alt=""/>
-                            </li>
-                            <li>
-                                韩都衣舍2017版女装春装木耳边卡通刺绣显瘦连衣裙
-                            </li>
-                        </ul>
-                    </div>
+                <div>
+                    {
+                        this.state.aGoods !== null && this.state.aGoods.map((val, key)=>{
+                            return (
+                                <div key={key} className={Css['goods-wrap']}>
+                                    {/*标题*/}
+                                    <div className={Css['classify-name']}>{val.title}</div>
+                                    {/*内容*/}
+                                    <div className={Css['goods-item-wrap']}>
+                                        {
+                                            val.goods !== null && val.goods.map((val1, key1)=>{
+                                                return (
+                                                    <ul key={key1}>
+                                                        <li className={Css['image']}>
+                                                            <img src={val1.image} alt=""/>
+                                                        </li>
+                                                        <li>
+                                                            {val1.title}
+                                                        </li>
+                                                    </ul>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
         );
