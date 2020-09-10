ea5a484c51c5bd7580b9d2813f96cd2a8408fbc8
心叶 <1329132389@qq.com>
2020-09-10 11:10:27 +0800

    完成无限下拉加载+销量和价格排序+搜索页面跳转

--- src/assets/js/conf/config.js
+++ src/assets/js/conf/config.js
@@ -1,4 +1,4 @@
-let prodUrl="http://vueshop.glbuys.com";//生成环境
+let prodUrl="//vueshop.glbuys.com";//生成环境
 let devUrl="/proxy";//开发者环境
 let baseUrl=process.env.NODE_ENV==='development'?devUrl:prodUrl;
 export default {
--- src/pages/home/goods/search.js
+++ src/pages/home/goods/search.js
@@ -2,23 +2,57 @@ import React from 'react';
 import config from '../../../assets/js/conf/config.js';
 import {request} from "../../../assets/js/libs/request";
 import IScroll from '../../../assets/js/libs/iscroll.js';
-import {lazyImg,localParam} from '../../../assets/js/utils/util.js';
+import Uprefresh from '../../../assets/js/libs/uprefresh';
 import Css from '../../../assets/css/home/goods/search.css';
+import SearchComponent from "../../../components/search/search";
+import {lazyImg} from "../../../assets/js/utils/util";
 export default class  GoodsSearch extends React.Component{
     constructor(props){
         super(props);
         this.state={
             screenMove: "",
-            bMask: false
-        }
+            bMask: false,
+            pageStyle: {display: "none"},
+            aGoodsList: [],
+            bPriceMenu: false,
+            bSaleMenu: false,
+            aPriceOrder: [
+                {
+                    title: "综合",
+                    type: "all",
+                    checked: true
+                },
+                {
+                    title: "从低到高",
+                    type: "up",
+                    checked: false
+                },
+                {
+                    title: "从高到低",
+                    type: "down",
+                    checked: false
+                }
+            ]
+        };
         this.myScroll = null;
+        this.oUpRefresh = null;
+        this.curPage = 1;
+        this.maxPage = 0;
+        this.oType = "all";
+        this.offsetBottom = 100;
+        this.bPriceMenu = false;
+        this.bSaleMenu = false;
+        this.sParams = "";
     }
     componentDidMount(){
+        // 兼容IOS的侧边栏
         this.myScroll = new IScroll("#screen",{
             scrollX: false,
             scrollY: true,
-            preventDefault: false
+            preventDefault: false,
+            aGoodsList: []
         });
+        this.getPageData();
     }
 
     // 打开抽屉
@@ -42,8 +76,129 @@ export default class  GoodsSearch extends React.Component{
     hideScreen() {
         this.setState({
             screenMove: Css['unmove'],
-            bMask: false
+            bMask: false,
+            bPriceMenu: false,
+            bSaleMenu: false,
+        });
+    }
+
+    // 返回上一层
+    goBack() {
+        this.props.history.goBack();
+    }
+
+    // 显示/隐藏价格排序
+    handlePriceOrder() {
+        this.bSaleMenu = false;
+        this.setState({
+            bSaleMenu: false
+        });
+        if (!this.bPriceMenu){
+            this.bPriceMenu = true;
+            this.setState({
+                bPriceMenu: true
+            });
+        }else{
+            this.bPriceMenu = false;
+            this.setState({
+                bPriceMenu: false
+            });
+        }
+    }
+
+    // 显示/隐藏销量排序
+    handleSalesOrder() {
+        this.bPriceMenu = false;
+        this.setState({
+            bPriceMenu: false
+        });
+        if (!this.bSaleMenu){
+            this.setState({
+                bSaleMenu: true
+            });
+        }else{
+            this.setState({
+                bSaleMenu: false
+            });
+        }
+        this.oType="sales";
+        this.getPageData();
+    }
+
+    // 显示/隐藏搜索组件页面
+    changeSearch(){
+        this.setState({pageStyle:{display:"none"}})
+    }
+
+    // 显示/隐藏搜索组件页面
+    getStyle(val){
+        this.setState({pageStyle:val})
+    }
+
+    // 封装url请求链接
+    setParams() {
+        this.sParams = "/api/home/goods/search?kwords=裙&param=&&price1=&price2=&otype="+this.oType+"&cid=";
+    }
+
+    // 获取数据
+    getPageData() {
+        // 改变sParams方便下面使用
+        this.setParams();
+        let url = config.baseUrl + "/api/home/goods/search?"+this.sParams+"&page=1&token="+config.token;
+        request(url).then(res=>{
+            if (res.code === 200) {
+                this.setState({
+                    aGoodsList: res.data
+                });
+                this.maxPage = res.pageinfo.pagenum;
+                lazyImg();
+                this.getScrollPage();
+            }else{
+                this.setState({
+                    aGoodsList: []
+                })
+            }
         })
+    }
+
+    // 无限下拉刷新
+    getScrollPage() {
+        this.oUpRefresh = new Uprefresh({
+            "curPage": this.curPage,
+            "maxPage": this.maxPage,
+            "offsetBottom": this.offsetBottom
+        }, curPage => {
+            let url = config.baseUrl + "/api/home/goods/search?"+this.sParams+"&page="+curPage+"&cid=&token="+config.token;
+            request(url).then(res=>{
+                if (res.code === 200) {
+                    console.log(res);
+                    if (res.data.length>0){
+                        let aGoods = this.state.aGoodsList;
+                        for (let i=0; i<res.data.length; i++) {
+                            aGoods.push(res.data[i]);
+                        }
+                        this.setState({
+                            aGoodsList: aGoods
+                        })
+                    }
+                    lazyImg();
+                }
+            })
+        })
+    }
+
+    // 选择价格排序里面的值
+    checkPriceOrder(index) {
+        let aPriceOrder = this.state.aPriceOrder;
+        for (let key in aPriceOrder) {
+            aPriceOrder[key].checked = false
+        }
+        aPriceOrder[index].checked = true;
+        this.setState({
+            aPriceOrder: aPriceOrder
+        });
+        this.oType = aPriceOrder[index].type;
+        this.getPageData();
     }
 
     render(){
@@ -53,141 +208,62 @@ export default class  GoodsSearch extends React.Component{
                 <div className={Css['search-top']}>
                     {/*头部搜索盒子*/}
                     <div className={Css['search-header']}>
-                        <div className={Css['back']}></div>
+                        <div className={Css['back']} onClick={this.goBack.bind(this)}></div>
                         <div className={Css['search-wrap']}>
                             <div className={Css['search-icon']}></div>
-                            <div className={Css['search']}>请输入您的宝贝名称</div>
+                            <div className={Css['search']} onClick={this.changeSearch.bind(this)}>请输入您的宝贝名称</div>
                         </div>
                         <div className={Css['screen-btn']} onClick={this.showScreen.bind(this)}>筛选</div>
                     </div>
                     {/*排序盒子*/}
                     <div className={Css['order-main']}>
                         {/*筛选排序子盒子*/}
-                        <div className={Css['order-item']}>
+                        <div className={this.state.bPriceMenu ? Css['order-item']+ " " + Css['active'] : Css['order-item']} onClick={this.handlePriceOrder.bind(this)}>
                             <div className={Css['order-item-text']}>综合</div>
                             <div className={Css['order-icon']}></div>
                             {/*选择排序*/}
-                            <ul className={Css['order-menu'] + " hide"}>
-                                <li className={Css['active']}>综合</li>
-                                <li>价格从低到高</li>
-                                <li>价格从高到低</li>
+                            <ul className={this.state.bPriceMenu ? Css['order-menu'] : Css['order-menu'] + " hide"}>
+                                {
+                                    this.state.aPriceOrder.length > 0 ? this.state.aPriceOrder.map((item, index)=>{
+                                        return (
+                                            <li key={index} className={item.checked ? Css['active'] : ""} onClick={this.checkPriceOrder.bind(this, index)}>{item.title}</li>
+                                        )
+                                    }) : ""
+                                }
                             </ul>
                         </div>
-                        <div className={Css['order-item']}>
-                            <div className={Css['order-text']}>销量</div>
+                        <div className={this.state.bSaleMenu ? Css['order-item'] + " " + Css['active'] : Css['order-item']}>
+                            <div className={Css['order-item-text']} onClick={this.handleSalesOrder.bind(this)}>销量</div>
                         </div>
                     </div>
                 </div>
                 {/*盒子*/}
                 <div className={Css['goods-main']}>
-                    <div className={Css['goods-list']}>
-                        {/*缩略图*/}
-                        <div className={Css['image']}>
-                            <img src="//vueshop.glbuys.com/uploadfiles/1524554409.jpg" alt=""/>
-                        </div>
-                        {/*内容介绍*/}
-                        <div className={Css['goods-content']}>
-                            <div className={Css['goods-title']}>
-                                蕾丝半身裙女2019春夏新款韩版修身包臀a字纯色不规则裙 CM81004
-                            </div>
-                            <div className={Css['price']}>
-                                ￥118
-                            </div>
-                            <div className={Css['sales']}>
-                                销量<span>100</span>件
-                            </div>
-                        </div>
-                    </div>
-                    <div className={Css['goods-list']}>
-                        {/*缩略图*/}
-                        <div className={Css['image']}>
-                            <img src="//vueshop.glbuys.com/uploadfiles/1524554409.jpg" alt=""/>
-                        </div>
-                        {/*内容介绍*/}
-                        <div className={Css['goods-content']}>
-                            <div className={Css['goods-title']}>
-                                蕾丝半身裙女2019春夏新款韩版修身包臀a字纯色不规则裙 CM81004
-                            </div>
-                            <div className={Css['price']}>
-                                ￥118
-                            </div>
-                            <div className={Css['sales']}>
-                                销量<span>100</span>件
-                            </div>
-                        </div>
-                    </div>
-                    <div className={Css['goods-list']}>
-                        {/*缩略图*/}
-                        <div className={Css['image']}>
-                            <img src="//vueshop.glbuys.com/uploadfiles/1524554409.jpg" alt=""/>
-                        </div>
-                        {/*内容介绍*/}
-                        <div className={Css['goods-content']}>
-                            <div className={Css['goods-title']}>
-                                蕾丝半身裙女2019春夏新款韩版修身包臀a字纯色不规则裙 CM81004
-                            </div>
-                            <div className={Css['price']}>
-                                ￥118
-                            </div>
-                            <div className={Css['sales']}>
-                                销量<span>100</span>件
-                            </div>
-                        </div>
-                    </div>
-                    <div className={Css['goods-list']}>
-                        {/*缩略图*/}
-                        <div className={Css['image']}>
-                            <img src="//vueshop.glbuys.com/uploadfiles/1524554409.jpg" alt=""/>
-                        </div>
-                        {/*内容介绍*/}
-                        <div className={Css['goods-content']}>
-                            <div className={Css['goods-title']}>
-                                蕾丝半身裙女2019春夏新款韩版修身包臀a字纯色不规则裙 CM81004
-                            </div>
-                            <div className={Css['price']}>
-                                ￥118
-                            </div>
-                            <div className={Css['sales']}>
-                                销量<span>100</span>件
-                            </div>
-                        </div>
-                    </div>
-                    <div className={Css['goods-list']}>
-                        {/*缩略图*/}
-                        <div className={Css['image']}>
-                            <img src="//vueshop.glbuys.com/uploadfiles/1524554409.jpg" alt=""/>
-                        </div>
-                        {/*内容介绍*/}
-                        <div className={Css['goods-content']}>
-                            <div className={Css['goods-title']}>
-                                蕾丝半身裙女2019春夏新款韩版修身包臀a字纯色不规则裙 CM81004
-                            </div>
-                            <div className={Css['price']}>
-                                ￥118
-                            </div>
-                            <div className={Css['sales']}>
-                                销量<span>100</span>件
-                            </div>
-                        </div>
-                    </div>
-                    <div className={Css['goods-list']}>
-                        {/*缩略图*/}
-                        <div className={Css['image']}>
-                            <img src="//vueshop.glbuys.com/uploadfiles/1524554409.jpg" alt=""/>
-                        </div>
-                        {/*内容介绍*/}
-                        <div className={Css['goods-content']}>
-                            <div className={Css['goods-title']}>
-                                蕾丝半身裙女2019春夏新款韩版修身包臀a字纯色不规则裙 CM81004
-                            </div>
-                            <div className={Css['price']}>
-                                ￥118
-                            </div>
-                            <div className={Css['sales']}>
-                                销量<span>100</span>件
-                            </div>
-                        </div>
-                    </div>
+                    {
+                        this.state.aGoodsList.length > 0 ?
+                        this.state.aGoodsList.map((item, index) => {
+                            return (
+                                <div key={index} className={Css['goods-list']}>
+                                    {/*缩略图*/}
+                                    <div className={Css['image']}>
+                                        <img src={item.image} alt={item.title}/>
+                                    </div>
+                                    {/*内容介绍*/}
+                                    <div className={Css['goods-content']}>
+                                        <div className={Css['goods-title']}>
+                                            {item.title}
+                                        </div>
+                                        <div className={Css['price']}>
+                                            ￥{item.price}
+                                        </div>
+                                        <div className={Css['sales']}>
+                                            销量<span>{item.sales}</span>件
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        }) : ""
+                    }
                     <div className={Css['goods-list']}>
                         {/*缩略图*/}
                         <div className={Css['image']}>
@@ -373,6 +449,8 @@ export default class  GoodsSearch extends React.Component{
                         <div className={Css['item']+" "+Css['sure']}>确定</div>
                     </div>
                 </div>
+                {/*搜索组件*/}
+                <SearchComponent pageStyle={this.state.pageStyle} childStyle={this.getStyle.bind(this)}></SearchComponent>
             </div>
         );
     }
