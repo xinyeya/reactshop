a727ce4e0e3dd6a7463b3673fe3010c226a803b9
心叶 <1329132389@qq.com>
2020-09-14 11:44:35 +0800

    完成商品详情页面样式

--- src/pages/home/goods/details_item.js
+++ src/pages/home/goods/details_item.js
@@ -14,6 +14,10 @@ class DetailsItem extends React.Component {
     }
 
     componentDidMount() {
+        // 禁用遮罩层下的默认事件
+        this.refs['mask'].addEventListener('touchmove', function (e) {
+            e.preventDefault();
+        }, false);
         this.getSwiper();
     }
 
@@ -154,6 +158,121 @@ class DetailsItem extends React.Component {
                 <div className={Css['bottom-btn-wrap']}>
                     <div className={Css['btn']+" "+Css['fav']}>收藏</div>
                     <div className={Css['btn']+" "+Css['card']}>加入购物车</div>
+                </div>
+                {/*遮罩层*/}
+                <div ref={"mask"} className={Css['mask']}></div>
+                {/*控制面板*/}
+                <div ref={"cart-panel"} className={Css['cart-panel']}>
+                    {/*商品信息*/}
+                    <div className={Css['goods-info']}>
+                        {/*关闭按钮*/}
+                        <div className={Css['close-panel-wrap']}>
+                            <div className={Css['spot']}></div>
+                            <div className={Css['line']}></div>
+                            <div className={Css['close']}></div>
+                        </div>
+                        {/*缩略图*/}
+                        <div className={Css['goods-img']}>
+                            <img src="//vueshop.glbuys.com/uploadfiles/1524556409.jpg" alt=""/>
+                        </div>
+                        {/*商品信息盒子*/}
+                        <div className={Css['goods-wrap']}>
+                            <div className={Css['goods-title']}>
+                                高跟鞋女2018新款春季单鞋仙女甜美链子尖头防水台细跟女鞋一字带
+                            </div>
+                            {/*价格*/}
+                            <div className={Css['price']}>
+                                ￥128
+                            </div>
+                            {/*编码*/}
+                            <div className={Css['goods-code']}>
+                                商品编码：12342356
+                            </div>
+                        </div>
+                    </div>
+                    {/*数据面板*/}
+                    <div className={Css['attr-wrap']}>
+                        {/*数据列表*/}
+                        <div className={Css['attr-list']}>
+                            {/*标题*/}
+                            <div className={Css['attr-name']}>
+                                颜色
+                            </div>
+                            {/*选项盒子*/}
+                            <div className={Css['val-wrap']}>
+                                <span className={Css['val'] + " " + Css['active']}>灰色</span>
+                                <span className={Css['val']}>灰色</span>
+                                <span className={Css['val']}>灰色</span>
+                                <span className={Css['val']}>灰色</span>
+                                <span className={Css['val']}>灰色</span>
+                                <span className={Css['val']}>灰色</span>
+                            </div>
+                        </div>
+                        <div className={Css['attr-list']}>
+                            {/*标题*/}
+                            <div className={Css['attr-name']}>
+                                尺码
+                            </div>
+                            {/*选项盒子*/}
+                            <div className={Css['val-wrap']}>
+                                <span className={Css['val'] + " " + Css['active']}>35</span>
+                                <span className={Css['val']}>36</span>
+                                <span className={Css['val']}>37</span>
+                                <span className={Css['val']}>38</span>
+                                <span className={Css['val']}>39</span>
+                                <span className={Css['val']}>40</span>
+                            </div>
+                        </div>
+                        <div className={Css['attr-list']}>
+                            {/*标题*/}
+                            <div className={Css['attr-name']}>
+                                颜色
+                            </div>
+                            {/*选项盒子*/}
+                            <div className={Css['val-wrap']}>
+                                <span className={Css['val'] + " " + Css['active']}>灰色</span>
+                                <span className={Css['val']}>灰色</span>
+                                <span className={Css['val']}>灰色</span>
+                                <span className={Css['val']}>灰色</span>
+                                <span className={Css['val']}>灰色</span>
+                                <span className={Css['val']}>灰色</span>
+                            </div>
+                        </div>
+                        <div className={Css['attr-list']}>
+                            {/*标题*/}
+                            <div className={Css['attr-name']}>
+                                尺码
+                            </div>
+                            {/*选项盒子*/}
+                            <div className={Css['val-wrap']}>
+                                <span className={Css['val'] + " " + Css['active']}>35</span>
+                                <span className={Css['val']}>36</span>
+                                <span className={Css['val']}>37</span>
+                                <span className={Css['val']}>38</span>
+                                <span className={Css['val']}>39</span>
+                                <span className={Css['val']}>40</span>
+                            </div>
+                        </div>
+                    </div>
+                    {/*购买数量*/}
+                    <div className={Css['amount-wrap']}>
+                        {/*购买数量-文字*/}
+                        <div className={Css['amount-name']}>
+                            购买数量
+                        </div>
+                        {/*输入数量盒子*/}
+                        <div className={Css['amount-input-wrap']}>
+                            <div className={Css['btn'] + " " + Css['dec'] + " " + Css['active']}>-</div>
+                            <div className={Css['amount-input']}>
+                                <input type="tel" defaultValue={"1"}/>
+                            </div>
+                            <div className={Css['btn'] + " " + Css['inc']}>+</div>
+                        </div>
+                    </div>
+                    {/*确定按钮*/}
+                    <div className={Css['sure-btn']}>
+                        确定
+                    </div>
                 </div>
             </div>
         );
