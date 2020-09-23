94e39cf4725532cc6534cdbe06f103c752ef5169
心叶 <1329132389@qq.com>
2020-09-23 11:06:33 +0800

    选择收货地址页面的布局

--- src/pages/home/address/index.js
+++ src/pages/home/address/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import {connect} from 'react-redux';
 import config from "../../../assets/js/conf/config";
 import SubHeaderComponent from "../../../components/header/subheader";
+import Css from '../../../assets/css/home/address/index.css'
+import {safeAuth} from "../../../assets/js/utils/util";
 
 class AddressIndex extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {};
+        // safeAuth(props);
     }
 
     componentDidMount() {
@@ -25,9 +28,127 @@ class AddressIndex extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className={Css['page']}>
                 <SubHeaderComponent title={"选择收货地址"}></SubHeaderComponent>
-                <button style={{marginTop: "1rem"}} type={"button"} onClick={this.pushPage.bind(this, "address/add")}>添加收货地址</button>
+                <div className={Css['main']}>
+                    <div className={Css['address-nav']}>
+                        <div className={Css['address-nav-name-1']}>
+                            配送地址
+                        </div>
+                        <div className={Css['address-nav-name-2']} onClick={this.pushPage.bind(this, 'address/add')}>
+                            +添加收货地址
+                        </div>
+                    </div>
+                    {/*收货地址列表*/}
+                    <div className={Css['address-list']}>
+                        <div className={Css['address-info-wrap']+" "+Css['default']}>
+                            {/*默认图标*/}
+                            <div className={Css['check-mark']}></div>
+                            {/*地址信息*/}
+                            <div className={Css['address-info']}>
+                                <div className={Css['persion']}>
+                                    <span>张三</span>
+                                    <span>13717263515</span>
+                                </div>
+                                {/*默认位置*/}
+                                <div className={Css['address']}>
+                                    <span className={Css['default']}>默认</span>
+                                    <span className={Css['text']}>北京朝阳北京朝阳北京朝阳北京朝阳北京朝阳北京朝阳</span>
+                                </div>
+                            </div>
+                        </div>
+                        {/*操作*/}
+                        <div className={Css['handle-wrap']}>
+                            <div className={Css['edit']}></div>
+                            <div className={Css['del']}></div>
+                        </div>
+                    </div>
+                    {/*没有默认的*/}
+                    <div className={Css['address-list']}>
+                        <div className={Css['address-info-wrap']}>
+                            {/*默认图标*/}
+                            {/*地址信息*/}
+                            <div className={Css['address-info']}>
+                                <div className={Css['persion']}>
+                                    <span>张三</span>
+                                    <span>13717263515</span>
+                                </div>
+                                {/*默认位置*/}
+                                <div className={Css['address']}>
+                                    <span className={Css['text']}>北京朝阳北京朝阳北京朝阳北京朝阳北京朝阳北京朝阳</span>
+                                </div>
+                            </div>
+                        </div>
+                        {/*操作*/}
+                        <div className={Css['handle-wrap']}>
+                            <div className={Css['edit']}></div>
+                            <div className={Css['del']}></div>
+                        </div>
+                    </div>
+                    <div className={Css['address-list']}>
+                        <div className={Css['address-info-wrap']}>
+                            {/*默认图标*/}
+                            {/*地址信息*/}
+                            <div className={Css['address-info']}>
+                                <div className={Css['persion']}>
+                                    <span>张三</span>
+                                    <span>13717263515</span>
+                                </div>
+                                {/*默认位置*/}
+                                <div className={Css['address']}>
+                                    <span className={Css['text']}>北京朝阳北京朝阳北京朝阳北京朝阳北京朝阳北京朝阳</span>
+                                </div>
+                            </div>
+                        </div>
+                        {/*操作*/}
+                        <div className={Css['handle-wrap']}>
+                            <div className={Css['edit']}></div>
+                            <div className={Css['del']}></div>
+                        </div>
+                    </div>
+                    <div className={Css['address-list']}>
+                        <div className={Css['address-info-wrap']}>
+                            {/*默认图标*/}
+                            {/*地址信息*/}
+                            <div className={Css['address-info']}>
+                                <div className={Css['persion']}>
+                                    <span>张三</span>
+                                    <span>13717263515</span>
+                                </div>
+                                {/*默认位置*/}
+                                <div className={Css['address']}>
+                                    <span className={Css['text']}>北京朝阳北京朝阳北京朝阳北京朝阳北京朝阳北京朝阳</span>
+                                </div>
+                            </div>
+                        </div>
+                        {/*操作*/}
+                        <div className={Css['handle-wrap']}>
+                            <div className={Css['edit']}></div>
+                            <div className={Css['del']}></div>
+                        </div>
+                    </div>
+                    <div className={Css['address-list']}>
+                        <div className={Css['address-info-wrap']}>
+                            {/*默认图标*/}
+                            {/*地址信息*/}
+                            <div className={Css['address-info']}>
+                                <div className={Css['persion']}>
+                                    <span>张三</span>
+                                    <span>13717263515</span>
+                                </div>
+                                {/*默认位置*/}
+                                <div className={Css['address']}>
+                                    <span className={Css['text']}>北京朝阳北京朝阳北京朝阳北京朝阳北京朝阳北京朝阳</span>
+                                </div>
+                            </div>
+                        </div>
+                        {/*操作*/}
+                        <div className={Css['handle-wrap']}>
+                            <div className={Css['edit']}></div>
+                            <div className={Css['del']}></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
